71b5a5e92997eea63ffefa2738d95b583f093dbb
parent5446 <parent5446@00fa18db-6751-0410-88ac-f6e0d3fc553f>
2008-07-10 21:13:43 +0000

    Added ability to import XML dumps into database.

--- trunk/WikiBackup.i18n.php
+++ trunk/WikiBackup.i18n.php
@@ -22,22 +22,26 @@
 
 $messages = array(
 	'en' => array(
-		'backup'                   => 'Backup',
-		'backup-desc'              => 'Makes complete backups of the MediaWiki database.',
-		'backup-title'             => 'Backup the database',
-		'backup-submitted'         => 'Backup $1 has been submitted. Refresh the page occasionally to check its status.',
-		'backup-taken'             => 'Backup $1 is already running.',
-		'backup-deleted'           => 'Backup $1 has been deleted.',
-		'backup-header'            => 'Depending on the size of the wiki, making a backup can take a while.<br />\'\'\'Once deleted, backups cannot be recovered.\'\'\'',
-		'backup-job'               => '$1 [[User:$2|$2]] ([[User talk:$2|talk]] | [[Special:Contributions/$2|contribs]]) created Backup $3. Current Status: [$5$4] $6',
-		'backup-footer'            => '',
-		'backup-notify'            => 'A <a href="$1/Special:Backup">backup</a> you scheduld has completed.',
-		'backup-email-desc'        => 'Backup Emails:',
-		'backup-email-label'       => 'Allow emails to you about completed backups you started.' )
-		'backup-log-create'        => 'Backup creation log',
-		'backup-log-header-create' => 'A log of all new backups.',
-		'backup-log-entry-create'  => 'created backup $1',
-		'backup-log-delete'        => 'Backup deletion log',
-		'backup-log-header-create' => 'A log of all backup deletions.',
-		'backup-log-entry-create'  => 'deleted backup $1'
+		'backup'                  => 'Backup',
+		'backup-desc'             => 'Makes complete backups of the MediaWiki database.',
+		'backup-title'            => 'Backup the database',
+		'backup-submitted'        => 'Backup $1 has been submitted. Refresh the page occasionally to check its status.',
+		'backup-taken'            => 'Backup $1 is already running.',
+		'backup-imported'         => 'Backup $1 is being imported into the database.',
+		'backup-deleted'          => 'Backup $1 has been deleted.',
+		'backup-header'           => 'Depending on the size of the wiki, making a backup can take a while.<br />\'\'\'Once deleted, backups cannot be recovered.\'\'\'',
+		'backup-job'              => '$1 [[User:$2|$2]] ([[User talk:$2|talk]] | [[Special:Contributions/$2|contribs]]) created Backup $3. Current Status: [$5$4] $6',
+		'backup-footer'           => '',
+		'backup-notify'           => 'A <a href="$1/Special:Backup">backup</a> you scheduld has completed.',
+		'backup-email-desc'       => 'Backup Emails:',
+		'backup-email-label'      => 'Allow emails to you about completed backups you started.' )
+		'backup-log-create'       => 'Backup creation log',
+		'backup-log-create-text'  => 'A log of all new backups.',
+		'backup-log-create-entry' => 'created backup $1',
+		'backup-log-delete'       => 'Backup deletion log',
+		'backup-log-delete-text'  => 'A log of all backup deletions.',
+		'backup-log-delete-entry' => 'deleted backup $1',
+		'backup-log-import'       => 'Backup import log',
+		'backup-log-import-text'  => 'A log of each time an XML dump was imported into the database.',
+		'backup-log-import-entry' => 'imported backup $1'
 );
