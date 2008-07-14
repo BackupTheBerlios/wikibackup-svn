24be511022dfd72133c6ca4da57072849977c14d
parent5446 <parent5446@00fa18db-6751-0410-88ac-f6e0d3fc553f>
2008-07-14 02:51:55 +0000

    Added notes for the WikiBackup_body.php file.

--- trunk/WikiBackup_body.php
+++ trunk/WikiBackup_body.php
@@ -20,12 +20,40 @@
 
 ****************************************************************************/
 
+/**
+ * The main class for the Backup Special page.
+ * It interacts with the WikiBackup class as
+ * well as the MediaWiki interface to show job
+ * lists, initiate backups, etc.
+ *
+ * @author Tyler Romeo
+ * @version 0.5
+ */
 class SpecialBackup extends SpecialPage {
+
+	/**
+	 * Constructor for the SpecialBackup class.
+	 * Initializes the SpecialPage base class and
+	 * loads the extension messages.
+	 */
 	public function SpecialBackup() {
 		SpecialPage::SpecialPage( "Backup", 'mysql-backup' );
 		wfLoadExtensionMessages( 'SpecialBackup' );
         }
 
+	/**
+	 * Function called when generating the special page.
+	 * Overloads SpecialPage::execute, and uses the action
+	 * POST/GET parameter to determine what to do. Actual
+	 * HTML for Special page is in
+	 * {@link SpecialBackup#mainBackupPage( $msg = false, $error = 'error' )
+	 *  mainBackupPage function}.
+	 *
+	 * @param $par The virtual subpage of the Special page
+	 *             requested by the user.
+	 *
+	 * @return Returns bool depending on what the action is.
+	 */
 	public function execute( $par ) {
 		$this->setHeaders();
 		global $wgRequest, $wgUser, $wgOut;
@@ -45,6 +73,19 @@ class SpecialBackup extends SpecialPage {
 		}
 	}
 
+	/**
+	 * Uses the WikiBackup class to initate a backup.
+	 * First runs some tests on POST variables supplied
+	 * by the calling function before running the backup.
+	 *
+	 * @param $jobId The specific jobid to run.
+	 * @param $test  The value of the submit button on
+	 *               the original "New Backup" button. Used
+	 *               for testing.
+	 *
+	 * @return Returns the return value of the mainBackupPage
+	 *         function, which is most likely true;
+	 */
 	private function executeBackup( $jobId = false, $test = false ) {
 		global $wgUser;
 		$wgUser->load();
@@ -60,18 +101,49 @@ class SpecialBackup extends SpecialPage {
 		return $this->mainBackupPage( wfMsg( 'backup-submitted', $WikiBackup->backupId ), 'mw-lag-warn-normal' );
 	}
 
+	/**
+	 * Uses the WikiBackup class to import an existent backup.
+	 *
+	 * @param $backupId The ID of the backup to import.
+	 *
+	 * @return Returns the return value of the mainBackupPage
+	 *         function, which is most likely true.
+	 */
 	private function importBackup( $backupId ) {
 		$WikiBackup = new WikiBackup( $backupId );
 		$WikiBackup->import( $backupId );
 		return $this->mainBackupPage( wfMsg( 'backup-imported', $WikiBackup->backupId ), 'mw-lag-normal' );
 	}
 
+	/**
+	 * Uses the WikiBackup class to delete an existent backup.
+	 *
+	 * @param $backupId The ID of the backup to delete.
+	 *
+	 * @return Returns the return value of the mainBackupPage
+	 *         function, which is most likely true.
+	 */
 	private function deleteBackup( $backupId ) {
 		$WikiBackup = new WikiBackup( $backupId );
 		$WikiBackup->delete( $backupId );
 		return $this->mainBackupPage( wfMsg( 'backup-deleted', $WikiBackup->backupId ), 'mw-lag-warn-normal' );
 	}
 
+	/**
+	 * The function to generate the actual HTML for the special page.
+	 * First generates an error message at the top using the function's
+	 * parameters. Then it generates the header and new backup form.
+	 * It uses the WikiBackup class to generate a job list, and then put
+	 * it into HTML form. Finally, it adds the footer HTML, and uses the
+	 * clearHeaderMessage function to clear any backup completion messages.
+	 *
+	 * @param $msg   The error message to show at the top of the page. Does
+	 *               not show if not set.
+	 * @param $error The class for the error div that holds the error message
+	 *               at the top of the page. The default it 'error'.
+	 *
+	 * @return Always returns true.
+	 */ 
 	private function mainBackupPage( $msg = false, $error = 'error' ) {
 		global $wgOut;
 		$WikiBackup = new WikiBackup();
@@ -105,6 +177,14 @@ class SpecialBackup extends SpecialPage {
 		return true;
 	}
 
+	/**
+	 * Changes a key in the database to stop the completed
+	 * backup message from showing when the user has visited
+	 * this special page.
+	 *
+	 * @return Returns true if the database update was successful.
+	 *         False, otherwise.
+	 */
 	private function clearHeaderMessage() {
 		global $wgUser;
 		$dbw =& wfGetDB( DB_MASTER );
@@ -112,7 +192,23 @@ class SpecialBackup extends SpecialPage {
 	}
 }
 
+/**
+ * This class starts, deletes, and imports database backups
+ * for the MediaWiki engine.
+ *
+ * @author Tyler Romeo
+ * @version 0.5
+ */
 class WikiBackup {
+
+	/**
+	 * Universal constructor for the class. If a backup id is
+	 * given through the functions only parameter, it sets it
+	 * as a class variable. If not, it queries the database for
+	 * the next unused backup id, and sets that.
+	 *
+	 * @param $backupId The backup id to set for the class.
+	 */
 	public function __construct( $backupId = false ) {
 		$dbr =& wfGetDB( DB_SLAVE );
 		if( !$backupId ) {
@@ -126,6 +222,16 @@ class WikiBackup {
 		$this->backupId = $backupId;
 	}
 
+	/**
+	 * Generates a list of parameters and executes a backup
+	 * using the {@link WikiBackup#execInBackground( $path, $exe, $args = "" ) 
+	 * execInBackground function} to execute the backup script.
+	 * After starting the backup, it adds an entry to the log
+	 * noting so. Before generating the parameter list, it runs
+	 * the BeforeBackupCreation hook.
+	 *
+	 * @param $user The User object for the user starting the backup.
+	 */
 	public function execute( $user ) {
 		global $wgBackupPath, $wgBackupName, $IP, $wgDBserver, $wgDBport, $wgDBuser, $wgDBpassword, $wgDBname, $wgDBprefix, $wgBackupSleepTime, $wgEmergencyContact;
 		$user->load(); $UserCanEmail = ( $user->isAllowed( 'mysql-backup' ) && $user->isEmailConfirmed() && $user->getOption( 'wpBackupEmail' ) );
@@ -137,6 +243,14 @@ class WikiBackup {
 		$LogPage->addEntry( 'backup', Title::newFromText( "Special:Backup" ), "", array( $this->backupId ) );
 	}
 
+	/**
+	 * Imports a specific backup into the database. Before generating
+	 * a parameter list for the background script, the BeforeBackupImport
+	 * hook is run.
+	 *
+	 * @param $backupId The backup id to import if different from the one
+	 *                  stored within the class.
+	 */
 	public function import( $backupId = false ) {
 		if( !$backupId ) { $backupId = $this->backupId(); }
 		global $wgBackupPath, $wgBackupName, $IP, $wgDBserver, $wgDBport, $wgDBuser, $wgDBpassword, $wgDBname, $wgDBprefix, $wgBackupSleepTime, $wgReadOnlyFile;
@@ -147,6 +261,14 @@ class WikiBackup {
 		$LogPage->addEntry( 'backup-import', Title::newFromText( "Special:Backup" ), "", array( $this->backupId ) );
 	}
 
+	/**
+	 * Deletes the file and database entry associated with a backup.
+	 * Before deleting the backup, it runs the BeforeBackupDelete
+	 * hook, which can stop the deletion if it returns false.
+	 *
+	 * @param $backupId The backup id to delete if different from the one
+	 *                  stored within the class.
+	 */
 	public function delete( $backupId = false ) {
 		if( !$backupId ) { $backupId = $this->backupId(); }
 		global $wgBackupPath, $wgBackupName, $IP;
@@ -160,6 +282,14 @@ class WikiBackup {
 		$LogPage->addEntry( 'backup-delete', Title::newFromText( "Special:Backup" ), "", array( $backupId ) );
 	}
 
+	/**
+	 * Checks the status of a certain backup job and returns it.
+	 *
+	 * @param $backupId The backup id to check if different from the one
+	 *                  stored within the class.
+	 *
+	 * @return Returns the status of the backup job.
+	 */
 	public function checkJob( $backupId = false ) {
 		if( !$backupId ) { $backupId = $this->backupId(); }
 		$dbr =& wfGetDB( DB_SLAVE );
@@ -168,6 +298,12 @@ class WikiBackup {
 		return $status;
 	}
 
+	/**
+	 * Generates a zero-point array of all open and completed backups.
+	 * Each key has another array containing the backup information.
+	 *
+	 * @return Returns an array of backup information.
+	 */
 	public function generateJobList() {
 		$dbr =& wfGetDB( DB_SLAVE );
 		$res = $dbr->selectRow( "backups", "*", "", "WikiBackup::generateJobList", array( "GROUP BY" => "timestamp" ) );
@@ -178,6 +314,12 @@ class WikiBackup {
 		return $jobs;
 	}
 
+	/**
+	 * Runs a certain process in the background.
+	 *
+	 * @param $path The physical path of the program to run.
+	 * @param $exe  The name of the program to run.
+	 * @param $args The arguments to feed to the program.
 	private function execInBackground( $path, $exe, $args = "" ) {
 		global $conf;
 		if( file_exists( $path . $exe ) ) {
